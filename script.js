675dfdf90c05aa508be8b224833861c82a86cd40
OlenaNovikova <novikovaolena13@gmail.com>
2025-06-17 22:26:11 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-
-	function changeBackground() {
+function changeBackground() {
     document.body.style.backgroundColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Сайт успішно завантажено!");
+});
 
 
